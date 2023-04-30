85045688e504e1ec2a4b3e73128bf235577c1a94
Josh Cline <joshccline@outlook.com>
2023-04-29 20:42:44 -0400

    Added 404 Page route.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const env = require('dotenv').config()
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen('3000')
